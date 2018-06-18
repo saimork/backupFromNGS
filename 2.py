dd1b02db8b323468734342f8eab3aa16295cda71
saimork <benz@kkumail.com>
2018-06-18 10:31:12 +0700

    v1.0

--- 2.py
+++ 2.py
@@ -27,15 +27,12 @@ logger.info('start time : %s' %startTime)
 def command(command):
     stdin, stdout, stderr = client.exec_command(command)
 
+# input ip, username and password to login
 host=input('IP Server you want to connect : ')
 port=22
 user=input('User to login : ')
 passw=getpass.getpass('password : ')
 
-# host='192.168.111.112'
-# port=22
-# user='mork'
-# passw='rs43392'
 #connect ssh
 try:
     client = paramiko.SSHClient()
@@ -43,6 +40,7 @@ try:
     client.connect(host,username=user,password=passw)
 except paramiko.SSHException:
     logger.exception('ssh connection error')
+
 #connect sftp
 try:
     transport = paramiko.Transport((host,port))
@@ -50,11 +48,13 @@ try:
     sftp = paramiko.SFTPClient.from_transport(transport)
 except paramiko.SFTP_FAILURE:
     logger.exception('sftp connection error')
-#check file and folder
+
+
 serverPath = '/home/mork/backupMe'
 for fileInResultDir in tqdm(sftp.listdir_attr(serverPath),ascii=True,ncols=100): #for every file in backup directory
     if stat.S_ISDIR(fileInResultDir.st_mode) and 'URRC' in fileInResultDir.filename\
             and not('_tn_' in fileInResultDir.filename): #select target directory
+
         wd1 = 'e:/URRC/pycharmProject/backupServer/backupArea/'
         wd2 = 'e:/URRC/pycharmProject/backupServer/backupArea/%s/' % fileInResultDir.filename
         bam = wd2 + 'BAM/'
@@ -63,105 +63,139 @@ for fileInResultDir in tqdm(sftp.listdir_attr(serverPath),ascii=True,ncols=100):
         plugin_outServ = fileServerPath + 'plugin_out/'
         filenameTar = fileInResultDir.filename[fileInResultDir.filename.index('URRC-') \
                                                :fileInResultDir.filename.index('URRC-') + 7]
+        logger.info('work directory: %s' %fileServerPath)
 
-        def downloadCoverAverage(): #download coverAverage.tar.gz
+        # download coverAverage.tar.gz
+        def downloadCoverAverage():
             try:
                 sftp.get(plugin_outServ + 'coverAverage-%s.tar.gz' % filenameTar, \
                          plugin_out + 'coverAverage-%s.tar.gz' % filenameTar)
                 logger.info('download coverAverage complete')
             except IOError:
                 logger.error('file not exist')
+                pass
 
-        def downloadVariant(): #download variant.tar.gz
+        # download variant.tar.gz
+        def downloadVariant():
             try:
                 sftp.get(plugin_outServ + 'variant-%s.tar.gz' % filenameTar, \
                          plugin_out + 'variant-%s.tar.gz' % filenameTar)
                 logger.info('download variant complete')
             except IOError:
                 logger.error('file not exist')
-        def tarCoverAverage(): #compress all coverAverage directory
+                pass
+
+        # compress all coverAverage directory
+        def tarCoverAverage():
             try:
                 command('cd %s ; tar -czf coverAverage-%s.tar.gz --anchored cov*' \
                         % (plugin_outServ, filenameTar))
-                time.sleep(1)
+                time.sleep(0.1)
                 logger.info('compress coverAverage complete')
             except IOError:
                 logger.error('file not exist')
-        def tarVariant(): #compress all variant directory
+                pass
+
+        # compress all variant directory
+        def tarVariant():
             try:
                 command('cd %s ; tar -czf variant-%s.tar.gz --anchored var*' \
                         % (plugin_outServ, filenameTar))
-                time.sleep(1)
+                time.sleep(0.1)
                 logger.info('compress variant complete')
             except IOError:
                 logger.error('file not exist')
-        if fileInResultDir.filename in os.listdir(wd1) :  #if already folder ่ just check file
+                pass
+
+        # if already folder in client just check file
+        # if not exist make directory
+        if fileInResultDir.filename in os.listdir(wd1) :  # if folder exist in client
             logger.info('folder : %s already exist' %fileInResultDir.filename)
             for fileInRun in sftp.listdir_attr(fileServerPath):
+                # if not exist folder "BAM" in client, make it
                 if not ('BAM' in os.listdir(wd2)):
                     os.mkdir(bam)
-                    logger.info('creat BAM folder complete')
+                    logger.info('create BAM folder complete')
+                # if not exist folder "plugin_out" in client, make it
                 elif not ('plugin_out' in os.listdir(wd2)):
                     os.mkdir(wd2+'plugin_out')
                     logger.info('create plugin_out folder complete')
+                # if report.pdf in client not exist, download
                 elif not('report.pdf' in os.listdir(wd2)) and fileInRun.filename == 'report.pdf':
                     sftp.get(fileServerPath+fileInRun.filename,wd2+fileInRun.filename)
                     logger.info('download report.pdf complete')
+
+                # check if exist file in client
+                # if file site not equal, download again
                 elif 'BAM' in os.listdir(wd2) and 'bam' in fileInRun.filename:
+                    # check file *.bam between server and client if size equal,it same file
                     if fileInRun.filename in os.listdir(bam) and 'bam' in fileInRun.filename:
                         if fileInRun.st_size == os.path.getsize(bam+fileInRun.filename):
                             logger.info('%s already exist' % fileInRun.filename)
                         else:
                             sftp.get(fileServerPath+'/'+fileInRun.filename,bam+fileInRun.filename)
                             logger.info('download %s complete' % fileInRun.filename)
+                    # if bam not exist in client, download it
                     elif not(fileInRun.filename in os.listdir(bam)) and 'bam' in fileInRun.filename:
                         sftp.get(fileServerPath +'/' + fileInRun.filename, bam + fileInRun.filename)
                         logger.info('download %s complete' %fileInRun.filename)
                     else :
                         pass
+
+                # check file report.pdf
                 elif 'report.pdf' in os.listdir(wd2) and 'report' in fileInRun.filename:
                     if fileInRun.st_size == os.path.getsize(wd2+fileInRun.filename):
                         logger.info('%s already exist' % fileInRun.filename)
                     else:
                         sftp.get(fileServerPath+fileInRun.filename,wd2+fileInRun.filename)
                         logger.info('download %s complete' %fileInRun.filename)
+
+                # check folder plugin_out
                 elif 'plugin_out' in fileInRun.filename:
-                    i = 0
                     covertargz = 0  # 0=not exist, 1=exist
                     vartargz = 0
-
+                    fileCover =0
+                    fileVariant = 0
                     for fileinPlugin in sftp.listdir_attr(plugin_outServ):
-                        if ('cover' in fileinPlugin.filename and '.tar.gz' in fileinPlugin.filename) and 'cover' in \
-                                sftp.listdir(plugin_outServ)[i] and '.tar.gz' in sftp.listdir(plugin_outServ)[i]:
+                        if ('cover' in fileinPlugin.filename and '.tar.gz' in fileinPlugin.filename):
                             covertargz += 1
-                        elif ('var' in fileinPlugin.filename and '.tar.gz' in fileinPlugin.filename) and 'var' in \
-                                sftp.listdir(plugin_outServ)[i] and '.tar.gz' in sftp.listdir(plugin_outServ)[i]:
+                        elif ('var' in fileinPlugin.filename and '.tar.gz' in fileinPlugin.filename):
                             vartargz += 1
+                        elif 'cover' in fileinPlugin.filename and not('.tar.gz' in fileinPlugin.filename):
+                            fileCover +=1
+                        elif 'variant' in fileinPlugin.filename and not('.tar.gz' in fileinPlugin.filename):
+                            fileVariant +=1
                         else:
                             pass
-                        i += 1
-                    if covertargz == 1 and vartargz == 1:
-                        logger.info('both .tar.gz already exist')
-                        downloadVariant()
-                        downloadCoverAverage()
-                    elif covertargz == 1 and vartargz == 0:
-                        logger.info('only coverAverage compressed')
-                        tarVariant()
-                        downloadVariant()
-                        downloadCoverAverage()
-                    elif covertargz == 0 and vartargz == 1:
-                        logger.info('only variant compressed')
-                        tarCoverAverage()
-                        downloadVariant()
-                        downloadCoverAverage()
-                    elif covertargz == 0 and vartargz == 0:
-                        logger.info('both not compress yet')
-                        tarCoverAverage()
-                        tarVariant()
-                        downloadVariant()
-                        downloadCoverAverage()
 
-                    elif 'plugin_out' in fileInRun.filename and 'plugin_out' in os.listdir(wd2) and len(
+                    # if plugin_out in server is empty, do nothing
+                    if fileCover == 0 and fileVariant == 0:
+                        logger.error('coverAverage and variant not exist')
+                        pass
+                    # if exist both directory and not compress, compress and download
+                    elif fileCover >= 1 and fileVariant >=1 and covertargz ==0 and vartargz == 0:
+                        tarCoverAverage()
+                        tarVariant()
+                        downloadCoverAverage()
+                        downloadVariant()
+                    # if exist only variant and compress not yet, compress and download
+                    elif fileCover == 0 and fileVariant >= 1 and vartargz == 0:
+                        logger.error('coverAverage not exist')
+                        tarVariant()
+                        downloadVariant()
+                    # if coverAverage only exist and compress not yet, compress and download
+                    elif fileCover >= 1 and fileVariant == 0 and covertargz == 0:
+                        logger.error('variant not exist')
+                        tarCoverAverage()
+                        downloadCoverAverage()
+                    # if both targz exist, download
+                    elif covertargz == 1 and vartargz ==1 :
+                        logger.info('both targz are exist')
+                        downloadCoverAverage()
+                        downloadVariant()
+
+                    # check file and download .tar.gz
+                    elif 'plugin_out' in fileInRun.filename and 'plugin_out' in os.listdir(wd2) and len(\
                         os.listdir(plugin_out)) != 0:
                         for fileinPlugin in sftp.listdir_attr(plugin_outServ):
                             if fileinPlugin.filename in os.listdir(plugin_out):
@@ -183,6 +217,9 @@ for fileInResultDir in tqdm(sftp.listdir_attr(serverPath),ascii=True,ncols=100):
                         pass
                 else:
                     pass
+
+        # if folder not exist in client
+        # make folder, compress file and download
         else:
             logger.info('not found %s folder' %fileInResultDir.filename)
             os.mkdir(wd2)
@@ -191,6 +228,8 @@ for fileInResultDir in tqdm(sftp.listdir_attr(serverPath),ascii=True,ncols=100):
             logger.info('create folder BAM in %s complete' % fileInResultDir.filename)
             os.mkdir(plugin_out)
             logger.info('create folder plugin_out in %s complete' % fileInResultDir.filename)
+
+            # download file
             for fileInEachRun in sftp.listdir_attr(fileServerPath):
                 if '.bam' in fileInEachRun.filename:
                     sftp.get(fileServerPath+fileInEachRun.filename, bam+fileInEachRun.filename)
@@ -199,50 +238,52 @@ for fileInResultDir in tqdm(sftp.listdir_attr(serverPath),ascii=True,ncols=100):
                     sftp.get(fileServerPath+fileInEachRun.filename, wd2+fileInEachRun.filename)
                     logger.info('doenload %s complete' %fileInEachRun.filename)
                 elif 'plugin_out' in fileInEachRun.filename:
-                    i = 0
                     covertargz = 0  # 0=not exist, 1=exist
                     vartargz = 0
+                    fileCover = 0
+                    fileVariant = 0
                     for fileinPlugin in sftp.listdir_attr(plugin_outServ):
-                        if ('cover' in fileinPlugin.filename and '.tar.gz' in fileinPlugin.filename) and 'cover' in \
-                                sftp.listdir(plugin_outServ)[i] and '.tar.gz' in sftp.listdir(plugin_outServ)[i]:
+                        if ('cover' in fileinPlugin.filename and '.tar.gz' in fileinPlugin.filename):
                             covertargz += 1
-                        elif ('var' in fileinPlugin.filename and '.tar.gz' in fileinPlugin.filename) and 'var' in \
-                                sftp.listdir(plugin_outServ)[i] and '.tar.gz' in sftp.listdir(plugin_outServ)[i]:
+                        elif ('var' in fileinPlugin.filename and '.tar.gz' in fileinPlugin.filename):
                             vartargz += 1
+                        elif 'cover' in fileinPlugin.filename and not ('.tar.gz' in fileinPlugin.filename):
+                            fileCover += 1
+                        elif 'variant' in fileinPlugin.filename and not ('.tar.gz' in fileinPlugin.filename):
+                            fileVariant += 1
                         else:
                             pass
-                        i += 1
 
-                    if covertargz == 1 and vartargz == 1:
-                        logger.info('both .tar.gz already exist')
-                        downloadVariant()
-                        downloadCoverAverage()
-                    elif covertargz == 1 and vartargz == 0:
-                        logger.info('only coverAverage compressed')
-                        tarVariant()
-                        downloadVariant()
-                        downloadCoverAverage()
-                    elif covertargz == 0 and vartargz == 1:
-                        logger.info('only variant compressed')
-                        tarCoverAverage()
-                        downloadVariant()
-                        downloadCoverAverage()
-                    elif covertargz == 0 and vartargz == 0:
-                        logger.info('both not compress yet')
+                    if fileCover == 0 and fileVariant == 0:
+                        logger.error('coverAverage and variant not exist')
+                        pass
+                    elif fileCover >= 1 and fileVariant >= 1 and covertargz == 0 and vartargz == 0:
                         tarCoverAverage()
                         tarVariant()
-                        downloadVariant()
                         downloadCoverAverage()
+                        downloadVariant()
+                    elif fileCover == 0 and fileVariant >= 1 and vartargz == 0:
+                        logger.error('coverAverage not exist')
+                        tarVariant()
+                        downloadVariant()
+                    elif fileCover >= 1 and fileVariant == 0 and covertargz == 0:
+                        logger.error('variant not exist')
+                        tarCoverAverage()
+                        downloadCoverAverage()
+                    elif covertargz == 1 and vartargz == 1:
+                        logger.info('both targz are exist')
+                        downloadCoverAverage()
+                        downloadVariant()
                     else:
                         pass
-
                 else:
                     pass
     else:
         logger.info('%s is not target' % fileInResultDir.filename)
-    time.sleep(0.1)
     logger.info('backup complete')
     logger.info('time use : %s' %(datetime.now()-startTime))
+
+#close both connection
 sftp.close()
 client.close()
 
